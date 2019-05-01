271af735a89dcbcca1fc89a39e7f64c2fe20c0c8
Michał Żuk <michalzuk33@gmail.com>
2019-05-01 18:25:22 +0200

    Initial commit using react-native-boilerplate

---
+++ App/Containers/Example/ExampleScreen.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import { connect } from 'react-redux';
+import { PropTypes } from 'prop-types';
+import ExampleActions from 'App/Stores/Example/Actions';
+import { liveInEurope } from 'App/Stores/Example/Selectors';
+
+class ExampleScreen extends React.Component {
+  componentDidMount() {
+    this.props.fetchUser();
+  }
+
+  render() {
+    return (
+      <View>
+        <Text>XD</Text>
+      </View>
+    );
+  }
+}
+
+ExampleScreen.propTypes = {
+  user: PropTypes.object,
+  userIsLoading: PropTypes.bool,
+  userErrorMessage: PropTypes.string,
+  fetchUser: PropTypes.func,
+  liveInEurope: PropTypes.bool,
+};
+
+const mapStateToProps = state => ({
+  user: state.example.user,
+  userIsLoading: state.example.userIsLoading,
+  userErrorMessage: state.example.userErrorMessage,
+  liveInEurope: liveInEurope(state),
+});
+
+const mapDispatchToProps = dispatch => ({
+  fetchUser: () => dispatch(ExampleActions.fetchUser()),
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ExampleScreen);
