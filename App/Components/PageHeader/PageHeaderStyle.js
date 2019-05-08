1c6d09505651b0264505ecaafb23166f57c4dbbb
Michał Żuk <michalzuk33@gmail.com>
2019-05-08 13:42:53 +0200

    changed layout of PageHeader

--- App/Components/PageHeader/PageHeaderStyle.js
+++ App/Components/PageHeader/PageHeaderStyle.js
@@ -5,9 +5,12 @@ import { Colors } from '../../Theme';
 
 export const StyledTitle = styled(Title)`
   padding-left: ${wp(6)};
-  padding-top: ${hp(4)};
+  padding-top: ${hp(2)}
   padding-bottom: ${hp(1)}px;
+  margin-top: 0;
   font-size: ${wp(8)};
+  background-color: ${Colors.black};
   text-transform: uppercase;
-  color: ${Colors.grey};
+  color: ${Colors.white};
+  font-family: 'AdventPro-Bold';
 `;
