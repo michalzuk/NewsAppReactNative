769798bd337f99710488797cff553aea2ae8ed57
Michał Żuk <michalzuk33@gmail.com>
2019-05-08 13:46:32 +0200

    Minor SingleArticle layout changes, replaced translation string (pl)

--- App/Components/SingleArticle/SingleArticleStyle.js
+++ App/Components/SingleArticle/SingleArticleStyle.js
@@ -3,10 +3,6 @@ import { Card, Paragraph, Text, Title } from 'react-native-paper';
 import { Colors } from '../../Theme';
 import { heightPercentageToDP as hp, widthPercentageToDP as wp } from 'react-native-responsive-screen';
 
-export const StyledCardContent = styled(Card.Content)`
-  padding: ${wp(1)}px;
-`;
-
 export const StyledCard = styled(Card)`
   margin-bottom: ${hp(4)};
   border-bottom-width: 1px;
@@ -15,15 +11,22 @@ export const StyledCard = styled(Card)`
   padding: 0 ${wp(2)}px ${hp(2)}px ${wp(2)}px;
 `;
 
-export const DetailsText = styled(Text)`
-  color: ${Colors.grey};
-`;
-
-export const StyledTitle = styled(Title)`
-  line-height: ${hp(3)};
+export const StyledCardContent = styled(Card.Content)`
+  padding: ${wp(1)}px;
+  padding-top: ${wp(2)};
 `;
 
 export const StyledDescription = styled(Paragraph)`
   padding-top: ${hp(1)}px;
   padding-left: ${wp(0.75)}px;
+  font-family: 'OpenSans-Light';
+`;
+
+export const DetailsText = styled(Text)`
+  color: ${Colors.grey};
+  font-family: 'OpenSans-Light';
+`;
+
+export const StyledTitle = styled(Title)`
+  line-height: ${hp(3.5)};
 `;
